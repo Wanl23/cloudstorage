48de658c97c7b15da3a43455e8f82c9874a72bc2
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-04-28 22:59:36 +0500

    download and send files and simple UI

---
+++ src/main/java/client/MainController.java
@@ -0,0 +1,85 @@
+package client;
+
+import common.AbstractMessage;
+import common.FileMessage;
+import common.FileRequest;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    TextField tfFileName;
+
+    @FXML
+    ListView<String> filesList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Network.start();
+        Thread t = new Thread(() -> {
+            try {
+                while (true) {
+                    AbstractMessage am = Network.readObject();
+                    if (am instanceof FileMessage) {
+                        FileMessage fm = (FileMessage) am;
+                        Files.write(Paths.get("client_storage/" + fm.getFilename()), fm.getData(), StandardOpenOption.CREATE);
+                        refreshLocalFilesList();
+                    }
+                }
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            } finally {
+                Network.stop();
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        refreshLocalFilesList();
+    }
+
+    public void pressOnDownloadBtn(ActionEvent actionEvent) throws IOException {
+        if (tfFileName.getLength() > 0) {
+            Network.sendMsg(new FileMessage(Paths.get("client_storage/" + tfFileName)));
+            tfFileName.clear();
+        }
+    }
+
+    public void pressOnSendBtn(ActionEvent actionEvent) throws IOException {
+        if (tfFileName.getLength() > 0) {
+            Network.sendMsg(new FileMessage(Paths.get("client_storage/" + tfFileName.getText())));
+            tfFileName.clear();
+        }
+    }
+
+    public void refreshLocalFilesList() {
+        if (Platform.isFxApplicationThread()) {
+            try {
+                filesList.getItems().clear();
+                Files.list(Paths.get("client_storage")).map(p -> p.getFileName().toString()).forEach(o -> filesList.getItems().add(o));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Platform.runLater(() -> {
+                try {
+                    filesList.getItems().clear();
+                    Files.list(Paths.get("client_storage")).map(p -> p.getFileName().toString()).forEach(o -> filesList.getItems().add(o));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
